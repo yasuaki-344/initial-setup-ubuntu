ccf56196fab0de2214900f31c1285e4f597fd3b1
yasuaki-miyoshi <ikausay.443@gmail.com>
2020-12-14 23:19:23 +0900

    Add script for setup of docker.

---
+++ setup-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo sevcie docker start
+sudo docker run hello-world
+sudo sevcie docker stop
